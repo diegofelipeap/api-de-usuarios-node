d836f0176b202e1c17b9d1cb6429705ff08f2e07
diegofelipeap <diegospd22@gmail.com>
2024-01-19 19:47:21 -0300

    Aprendendo sobre módulos no Node e exportação de módulos.

---
+++ modules/index.js
@@ -0,0 +1,5 @@
+const { multi, sum, person } = require('./objects&Functions')
+
+console.log(sum(2,8))
+console.log(multi(2,8))
+console.log(person.name)
---
+++ modules/objects&Functions.js
@@ -0,0 +1,11 @@
+function sum(firstNumber, secondNumber) {
+    return firstNumber + secondNumber
+}
+
+function multi(firstNumber, secondNumber) {
+    return firstNumber * secondNumber
+}
+
+const person = { name: "Diego Pereira", age: 24, address: "Center" }
+
+module.exports = { multi, sum, person } //Para exportar todos do módulo p/ outro.
